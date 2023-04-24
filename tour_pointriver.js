368da7b4dd7ead5a672520d9984cef3a1a615c62
eingfah <124545679+eingfah@users.noreply.github.com>
2023-04-24 11:53:33 +0700

    Add files via upload

---
+++ tour_pointriver.js
@@ -0,0 +1,155 @@
+var tour_pointriver = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "wat arun ratchawararam",
+          "type": "culture",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.48837282936114,
+            13.7440061493037
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Throne Hall of krung thonburi",
+          "type": "culture",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            100.48972786768172,
+            13.742712695394872
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Museum of the Emerald buddha temple",
+          "type": "special",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.4901366128982,
+            13.750976934358192
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "wang lang market",
+          "type": "special",
+          "level": 9
+        },
+        "geometry": {
+          "coordinates": [
+            100.48627981349125,
+            13.756045053241749
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "wat phra kaew",
+          "type": "cuture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.49269278835953,
+            13.75153250460157
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "saranrom park",
+          "type": "nulture",
+          "level": 8
+        },
+        "geometry": {
+          "coordinates": [
+            100.49513094383224,
+            13.748415535420818
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "museum of siam",
+          "type": "special",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.49390648879489,
+            13.743944922031659
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "mrt sanam chai",
+          "type": "special",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.49468200772435,
+            13.744325421466897
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "icon siam",
+          "type": "special",
+          "level": 10
+        },
+        "geometry": {
+          "coordinates": [
+            100.51042577116289,
+            13.726878258427448
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "chaophraya sky park",
+          "type": "nulture",
+          "level": 7
+        },
+        "geometry": {
+          "coordinates": [
+            100.49855738414453,
+            13.739076977754905
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
